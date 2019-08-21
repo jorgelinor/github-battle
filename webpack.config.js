df77cedbe127e21744f2c64a63ec8b5d12eea4d4
Jorgelin Ortiz <jorgar2000@gmail.com>
2019-08-20 20:03:14 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './app/index.js',
+    module: {
+        rules: [
+            { test: /\.svg$/, use: 'svg-inline-loader' },
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+            { test: /\.(js)$/, use: 'babel-loader' }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'app/index.html'
+        })
+    ],
+    mode: 'production',
+    devServer: {
+        historyApiFallback: true
+    }
+}
+
